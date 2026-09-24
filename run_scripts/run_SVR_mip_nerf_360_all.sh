685cc5c21a0bf6f52860ca43d66b23c3b3594e09
tamthuc1995 <dotamthuc@gmail.com>
2026-09-24 19:09:52 -0400

    Update run scripts and run SVCom test

--- run_scripts/run_SVR_mip_nerf_360_all.sh
+++ run_scripts/run_SVR_mip_nerf_360_all.sh
@@ -1,15 +1,16 @@
 export CUDA_VISIBLE_DEVICES="0"
 
-root_path=/home/dotamthuc/Works/Projects/Compress3DGS/Experiments
-path_dataset=$root_path/data/mip-nerf-360
+root_path=/home/dotamthuc/Works/Compress3DSplats/ResultsICASSP27
+svcomp_path=Differentiable3DPC-Decompression-and-Rendering/submodules/SVCompression
+path_dataset=/home/dotamthuc/Works/Compress3DSplats/ScenesData/mip-nerf-360
 
-path_model_base=$root_path/output/SVCompress/SVRasterTiny/mip-nerf-360
+path_model_base=$root_path/Models/SVCompress/SVRaster/mip-nerf-360
 
-path_bitstream_base=$root_path/output/SVCompress/SVRasterTinyQ2Compressed/mip-nerf-360
+path_bitstream_base=$root_path/Models/SVCompress/SVRasterCompressed/mip-nerf-360
 
-path_bitstream_dircolor_base=$root_path/output/SVCompress/SVRasterTinyQ2Compressed-DirColorv/mip-nerf-360
+path_bitstream_dircolor_base=$root_path/Models/SVCompress/SVRasterCompressed-DirColorv/mip-nerf-360
 
-path_bitstream_noshstransform_base=$root_path/output/SVCompress/SVRasterTinyQ2Compressed-NoShsTransform/mip-nerf-360
+path_bitstream_noshstransform_base=$root_path/Models/SVCompress/SVRasterCompressed-NoShsTransform/mip-nerf-360
 
 declare -a scene_list=(
     bicycle
@@ -25,8 +26,8 @@ declare -a scene_list=(
 
 
 declare -a color_coefs_stepsize=(
-    0.1
-    # 0.25
+    # 0.1
+    0.25
     # 0.5
     # 1
     # 2
@@ -36,8 +37,8 @@ declare -a color_coefs_stepsize=(
 )
 
 declare -a config_names=(
-    configRoot
-    # config00
+    # configRoot
+    config00
     # config01
     # config02
     # config03
@@ -53,16 +54,16 @@ for ((i = 0; i < ${#scene_list[@]}; i++)); do
 
     path_model="$path_model_base"/${scene_list[i]}
     echo path_model=$path_model
-    python -u ./svraster/train.py \
-        --eval \
-        --cfg_files ./svraster/cfg/mipnerf360_tunning.yaml \
-        --source_path $path_source \
-        --model_path $path_model \
-        --res_width 1600 
+    # python -u ./${svcomp_path}/train.py \
+    #     --eval \
+    #     --cfg_files ./${svcomp_path}/cfg/mipnerf360.yaml \
+    #     --source_path $path_source \
+    #     --model_path $path_model \
+    #     --res_width 1600 
 
-    python ./svraster/render.py $path_model --skip_train --eval_fps
-    python ./svraster/render.py $path_model --skip_train 
-    python ./svraster/eval.py $path_model 
+    # python ./${svcomp_path}/render.py $path_model --skip_train --eval_fps
+    # python ./${svcomp_path}/render.py $path_model --skip_train 
+    # python ./${svcomp_path}/eval.py $path_model 
 
 
     path_bitstream=$path_bitstream_base/${scene_list[i]}
@@ -71,47 +72,47 @@ for ((i = 0; i < ${#scene_list[@]}; i++)); do
         echo color_coefs_stepsize=${color_coefs_stepsize[j]}
         echo suffix=${config_names[j]}
 
-        python ./svraster/encode_bitstream.py $path_model \
+        python ./${svcomp_path}/encode_bitstream.py $path_model \
             --model_bitstreams_path $path_bitstream \
             --color_coefs_stepsize ${color_coefs_stepsize[j]} \
             --suffix ${config_names[j]} \
             --debug
 
-        python ./svraster/render_from_decode.py --model_bitstreams_path $path_bitstream --suffix ${config_names[j]} --skip_train
+        python ./${svcomp_path}/render_from_decode.py --model_bitstreams_path $path_bitstream --suffix ${config_names[j]} --skip_train
     done
 
 
-    path_bitstream_dircolor=$path_bitstream_dircolor_base/${scene_list[i]}
-    echo path_bitstream_dircolor=$path_bitstream_dircolor
-    for ((j = 0; j < ${#config_names[@]}; j++)); do
-        echo color_coefs_stepsize=${color_coefs_stepsize[j]}
-        echo suffix=${config_names[j]}
+    # path_bitstream_dircolor=$path_bitstream_dircolor_base/${scene_list[i]}
+    # echo path_bitstream_dircolor=$path_bitstream_dircolor
+    # for ((j = 0; j < ${#config_names[@]}; j++)); do
+    #     echo color_coefs_stepsize=${color_coefs_stepsize[j]}
+    #     echo suffix=${config_names[j]}
 
-        python ./svraster/encode_bitstream.py $path_model \
-            --model_bitstreams_path $path_bitstream_dircolor \
-            --color_coefs_stepsize ${color_coefs_stepsize[j]} \
-            --suffix ${config_names[j]} \
-            --use_directional_color_transform \
-            --debug
+    #     python ./${svcomp_path}/encode_bitstream.py $path_model \
+    #         --model_bitstreams_path $path_bitstream_dircolor \
+    #         --color_coefs_stepsize ${color_coefs_stepsize[j]} \
+    #         --suffix ${config_names[j]} \
+    #         --use_directional_color_transform \
+    #         --debug
 
-        python ./svraster/render_from_decode.py --model_bitstreams_path $path_bitstream_dircolor --suffix ${config_names[j]} --skip_train
-    done
+    #     python ./${svcomp_path}/render_from_decode.py --model_bitstreams_path $path_bitstream_dircolor --suffix ${config_names[j]} --skip_train
+    # done
 
-    path_bitstream_noshstransform=$path_bitstream_noshstransform_base/${scene_list[i]}
-    echo path_bitstream_noshstransform=$path_bitstream_noshstransform
-    for ((j = 0; j < ${#config_names[@]}; j++)); do
-        echo color_coefs_stepsize=${color_coefs_stepsize[j]}
-        echo suffix=${config_names[j]}
+    # path_bitstream_noshstransform=$path_bitstream_noshstransform_base/${scene_list[i]}
+    # echo path_bitstream_noshstransform=$path_bitstream_noshstransform
+    # for ((j = 0; j < ${#config_names[@]}; j++)); do
+    #     echo color_coefs_stepsize=${color_coefs_stepsize[j]}
+    #     echo suffix=${config_names[j]}
 
-        python ./svraster/encode_bitstream.py $path_model \
-            --model_bitstreams_path $path_bitstream_noshstransform \
-            --color_coefs_stepsize ${color_coefs_stepsize[j]} \
-            --suffix ${config_names[j]} \
-            --dont_do_shs_transform \
-            --debug
+    #     python ./${svcomp_path}/encode_bitstream.py $path_model \
+    #         --model_bitstreams_path $path_bitstream_noshstransform \
+    #         --color_coefs_stepsize ${color_coefs_stepsize[j]} \
+    #         --suffix ${config_names[j]} \
+    #         --dont_do_shs_transform \
+    #         --debug
 
-        python ./svraster/render_from_decode.py --model_bitstreams_path $path_bitstream_noshstransform --suffix ${config_names[j]} --skip_train
-    done
+    #     python ./${svcomp_path}/render_from_decode.py --model_bitstreams_path $path_bitstream_noshstransform --suffix ${config_names[j]} --skip_train
+    # done
 
 done
 
